9e7f4da850071ea689118a856b614205805f99f4
niko <niko@n.com>
2020-06-14 23:04:49 +0400

    survey model and mig created

---
+++ app/Survey.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class survey extends Model
+{
+    protected $fillable = [
+      'user_id', 'name', 'description'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
+}
