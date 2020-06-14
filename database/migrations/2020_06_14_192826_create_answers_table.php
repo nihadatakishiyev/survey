e309f7ec8b518c054269fc65898d070a0064c000
niko <niko@n.com>
2020-06-14 23:36:46 +0400

    answer added

---
+++ database/migrations/2020_06_14_192826_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->references('id')->on('questions');
+            $table->foreignId('responder_id')->references('id')->on('users');
+            $table->foreignId('option_id')->references('id')->on('options');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
